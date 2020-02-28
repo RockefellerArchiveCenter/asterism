f456d94422bd52aa5957e6e3e690795d9e0ca3cf
Patrick <pgalligan@rockarch.org>
2020-02-28 11:31:26 -0500

    change version
    Change version for testing

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='asterism',
-      version='0.3.2',
+      version='0.3',
       description='Helpers for Project Electron infrastructure',
       url='http://github.com/RockefellerArchiveCenter/asterism',
       author='Rockefeller Archive Center',
